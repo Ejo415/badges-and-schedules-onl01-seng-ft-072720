1d643e9cc4136e241b65125eecb81de86a3f1551
Eugene O'Neil <ejo7220@gmail.com>
2020-07-29 18:54:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,10 +21,10 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  batch_badge_creator(array).each do |id|
+  batch_badge_creator.each do |id|
      id
 
-  assign_rooms(array).each do |id|
+  assign_rooms.each do |id|
      id
   end
 end
