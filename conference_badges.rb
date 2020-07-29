114a819d92b1520a54c3a100437935b4b59780fe
Eugene O'Neil <ejo7220@gmail.com>
2020-07-29 18:36:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ def assign_rooms(array)
   rooms = []
   counter = 1
   array.each do |name|
-    rooms << "Hello, #{name}, You'll be assigned to room #{counter}!"
+    rooms << "Hello, #{name}! You'll be assigned to room #{counter}!"
     counter += 1 
   end
   rooms
